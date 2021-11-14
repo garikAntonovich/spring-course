63dfef7367d347ad789c58172ff478943df8feb7
garikAntonovich <garik.antonovich@gmail.com>
2021-11-14 06:25:33 +0300

    get java objects from db by id

---
+++ src/main/java/by/garikein/hibernate_test/Test2.java
@@ -0,0 +1,36 @@
+package by.garikein.hibernate_test;
+
+import by.garikein.hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            Employee employee = new Employee("Oleg", "Kizaru", "Hip-hop", 1337);
+            session.beginTransaction();
+            session.save(employee);
+//            session.getTransaction().commit();
+
+            int myId = employee.getId();
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+            Employee employee2 = session.get(Employee.class, myId);
+            session.getTransaction().commit();
+            System.out.println(employee2);
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
