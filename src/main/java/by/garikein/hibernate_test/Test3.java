6e079d0d26b42f70306d552601a329e8cd11bdd9
garikAntonovich <garik.antonovich@gmail.com>
2021-11-14 06:55:55 +0300

    get java objects from db by hql

---
+++ src/main/java/by/garikein/hibernate_test/Test3.java
@@ -0,0 +1,39 @@
+package by.garikein.hibernate_test;
+
+import by.garikein.hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            List<Employee> employeeList = session.createQuery("from Employee").getResultList();
+
+            List<Employee> employeeList = session.createQuery("from Employee where name = 'Alex' AND" +
+                    " salary>650").getResultList();
+
+            for (Employee employee : employeeList) {
+                System.out.println(employee);
+            }
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
