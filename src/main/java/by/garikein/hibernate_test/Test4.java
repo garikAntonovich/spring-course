1896bd29b9151e385e9e1d2d664ea458e333d974
garikAntonovich <garik.antonovich@gmail.com>
2021-11-15 03:43:19 +0300

    update java object from db

---
+++ src/main/java/by/garikein/hibernate_test/Test4.java
@@ -0,0 +1,32 @@
+package by.garikein.hibernate_test;
+
+import by.garikein.hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test4 {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            Employee employee = session.get(Employee.class, 7);
+//            employee.setSalary(1500);
+            session.createQuery("update Employee set salary = 1000 where name = 'Alex'").executeUpdate();
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
